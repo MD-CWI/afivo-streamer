e6e87a5eef673e281d1f3756dcbdc3b56f4378b5
Jannis Teunissen <jannis@teunissen.net>
2015-01-05 18:06:03 +0100

    fix restrict: now all levels

--- src/m_afivo_Xd.f90
+++ src/m_afivo_Xd.f90
@@ -1265,7 +1265,7 @@ contains
     integer, intent(in)       :: iv
     integer                   :: lvl
 
-    do lvl = tree%n_lvls-1, 1, -1
+    do lvl = tree%n_lvls-1, lbound(tree%lvls, 1), -1
        call a$D_restrict_to_boxes(tree%boxes, tree%lvls(lvl)%parents, iv)
     end do
   end subroutine a$D_restrict_tree
