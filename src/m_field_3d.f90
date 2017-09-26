9ccd9ee94e7741ff924e854efdb0e3075eb269d2
DanielRaposo <daniel@dhcp-99.eduroam.cwi.nl>
2017-09-26 12:00:58 +0200

    2D cyl and 3D not complete

---
+++ src/m_field_3d.f90
@@ -0,0 +1,322 @@
+module m_field_3d
+  use m_streamer
+  use m_a3_all
+  use m_flux_schemes_3d
+
+  implicit none
+  private
+
+  !> Start modifying the vertical background field after this time
+  real(dp) :: field_mod_t0 = 1e99_dp
+
+  !> Amplitude of sinusoidal modification
+  real(dp) :: field_sin_amplitude = 0.0_dp
+
+  !> Frequency (Hz) of sinusoidal modification
+  real(dp) :: field_sin_freq = 0.0_dp
+
+  !> Linear derivative of background field
+  real(dp) :: field_lin_deriv = 0.0_dp
+
+  !> Decay time of background field
+  real(dp) :: field_decay_time = huge(1.0_dp)
+
+  !> The applied electric field (vertical direction)
+  real(dp) :: field_amplitude = 1.0e6_dp
+
+  !> The applied voltage (vertical direction)
+  real(dp) :: field_voltage
+
+  !> Drop-off radius
+  real(dp) :: field_dropoff_radius = 1e-3_dp
+
+  !> Relative width over which the potential drops
+  real(dp) :: field_dropoff_relwidth = 0.5_dp
+
+  !> Location from which the field drops off (set below)
+  real(dp) :: field_dropoff_pos(2) = 0.0_dp
+
+  character(ST_slen) :: field_bc_type = "homogeneous"
+
+  public :: field_initialize
+  public :: field_compute
+  public :: field_from_potential
+  public :: field_get_amplitude
+  public :: field_set_voltage
+
+  public :: field_bc_homogeneous
+  public :: field_bc_dropoff_lin
+  public :: field_bc_dropoff_log
+
+contains
+
+  !> Set boundary conditions for the electric potential/field
+  subroutine field_initialize(cfg, mg)
+    type(CFG_t), intent(inout)  :: cfg !< Settings
+    type(mg3_t), intent(inout) :: mg  !< Multigrid option struct
+
+    call CFG_add_get(cfg, "field_mod_t0", field_mod_t0, &
+         "Modify electric field after this time (s)")
+    call CFG_add_get(cfg, "field_sin_amplitude", field_sin_amplitude, &
+         "Amplitude of sinusoidal modification (V/m)")
+    call CFG_add_get(cfg, "field_sin_freq", field_sin_freq, &
+         "Frequency of sinusoidal modification (Hz)")
+    call CFG_add_get(cfg, "field_lin_deriv", field_lin_deriv, &
+         "Linear derivative of field [V/(ms)]")
+    call CFG_add_get(cfg, "field_decay_time", field_decay_time, &
+         "Decay time of field (s)")
+    call CFG_add_get(cfg, "field_amplitude", field_amplitude, &
+         "The applied electric field (V/m) (vertical)")
+    call CFG_add_get(cfg, "field_bc_type", field_bc_type, &
+         "Type of boundary condition to use (homogeneous, ...)")
+
+    call CFG_add_get(cfg, "field_dropoff_radius", field_dropoff_radius, &
+         "Potential stays constant up to this radius")
+    call CFG_add_get(cfg, "field_dropoff_relwidth", field_dropoff_relwidth, &
+         "Relative width over which the potential drops")
+
+    field_voltage = -ST_domain_len * field_amplitude
+
+    select case (field_bc_type)
+    case ("homogeneous")
+       mg%sides_bc => field_bc_homogeneous
+    case ("dropoff_lin")
+       if (ST_cylindrical) then
+          field_dropoff_pos(:) = 0.0_dp
+       else
+          field_dropoff_pos(:) = 0.5_dp
+       end if
+
+       mg%sides_bc => field_bc_dropoff_lin
+    case ("dropoff_log")
+       if (ST_cylindrical) then
+          field_dropoff_pos(:) = 0.0_dp
+       else
+          field_dropoff_pos(:) = 0.5_dp
+       end if
+
+       mg%sides_bc => field_bc_dropoff_log
+    case default
+       error stop "field_bc_select error: invalid condition"
+    end select
+  end subroutine field_initialize
+
+  !> Compute electric field on the tree. First perform multigrid to get electric
+  !> potential, then take numerical gradient to geld field.
+  subroutine field_compute(tree, mg, have_guess)
+    use m_units_constants
+    type(a3_t), intent(inout) :: tree
+    type(mg3_t), intent(in)   :: mg ! Multigrid option struct
+    logical, intent(in)        :: have_guess
+    real(dp), parameter        :: fac = UC_elem_charge / UC_eps0
+    integer                    :: lvl, i, id, nc
+
+    nc = tree%n_cell
+
+    ! Set the source term (rhs)
+    !$omp parallel private(lvl, i, id)
+    do lvl = 1, tree%highest_lvl
+       !$omp do
+       do i = 1, size(tree%lvls(lvl)%leaves)
+          id = tree%lvls(lvl)%leaves(i)
+          tree%boxes(id)%cc(:, :, :, i_rhs) = fac * (&
+               tree%boxes(id)%cc(:, :, :, i_electron) - &
+               tree%boxes(id)%cc(:, :, :, i_pos_ion))
+       end do
+       !$omp end do nowait
+    end do
+    !$omp end parallel
+
+    call field_set_voltage(ST_time)
+
+    if (.not. have_guess) then
+       ! Perform a FMG cycle when we have no guess
+       call mg3_fas_fmg(tree, mg, .false., have_guess)
+    else
+       ! Perform cheaper V-cycles
+       do i = 1, ST_multigrid_num_vcycles
+          call mg3_fas_vcycle(tree, mg, .false.)
+       end do
+    end if
+
+    ! Compute field from potential
+    call a3_loop_box(tree, field_from_potential)
+
+    ! Set the field norm also in ghost cells
+    call a3_gc_tree(tree, i_eps, i_electric_fld, a3_gc_interp, a3_bc_neumann_zero)
+  end subroutine field_compute
+
+  !> Compute the electric field at a given time
+  function field_get_amplitude(time) result(electric_fld)
+    use m_units_constants
+    real(dp), intent(in) :: time
+    real(dp)             :: electric_fld, t_rel
+
+    t_rel = time - field_mod_t0
+    if (t_rel > 0) then
+       electric_fld = field_amplitude * exp(-t_rel/field_decay_time) + &
+            t_rel * field_lin_deriv + &
+            field_sin_amplitude * &
+            sin(t_rel * field_sin_freq * 2 * UC_pi)
+    else
+       electric_fld = field_amplitude
+    end if
+  end function field_get_amplitude
+
+  !> Compute the voltage at a given time
+  subroutine field_set_voltage(time)
+    use m_init_cond_3d
+    real(dp), intent(in) :: time
+
+    field_voltage = -ST_domain_len * field_get_amplitude(time)
+
+  end subroutine field_set_voltage
+
+  !> This fills ghost cells near physical boundaries for the potential
+  subroutine field_bc_homogeneous(box, nb, iv, bc_type, i_eps)
+    use m_init_cond_3d
+    type(box3_t), intent(inout)  :: box
+    integer, intent(in)           :: nb ! Direction for the boundary condition
+    integer, intent(in)           :: iv ! Index of variable
+    integer, intent(in), optional :: i_eps
+    integer, intent(out)          :: bc_type ! Type of boundary condition
+    integer                       :: nc
+
+    nc = box%n_cell
+
+    select case (nb)
+
+    case (a3_neighb_lowx)
+       bc_type = af_bc_neumann
+       box%cc(   0, 1:nc, 1:nc, iv) = 0
+    case (a3_neighb_highx)
+       bc_type = af_bc_neumann
+       box%cc(nc+1, 1:nc, 1:nc, iv) = 0
+    case (a3_neighb_lowy)
+       bc_type = af_bc_neumann
+       box%cc(1:nc,    0, 1:nc, iv) = 0
+    case (a3_neighb_highy)
+       bc_type = af_bc_neumann
+       box%cc(1:nc, nc+1, 1:nc, iv) = 0
+    case (a3_neighb_lowz)
+       bc_type = af_bc_dirichlet
+       box%cc(1:nc, 1:nc,    0, iv) = 0
+    case (a3_neighb_highz)
+       bc_type = af_bc_dirichlet
+       box%cc(1:nc, 1:nc, nc+1, iv) = field_voltage
+
+    end select
+
+  end subroutine field_bc_homogeneous
+
+  subroutine field_bc_dropoff_lin(box, nb, iv, bc_type, i_eps)
+    type(box3_t), intent(inout) :: box
+    integer, intent(in)          :: nb      ! Direction for the boundary condition
+    integer, intent(in)          :: iv      ! Index of variable
+    integer, intent(out)         :: bc_type ! Type of boundary condition
+    integer                      :: nc, i
+    integer, intent(in), optional   :: i_eps
+
+    integer                      :: j
+
+    real(dp)                     :: rr(3), rdist
+
+    nc = box%n_cell
+
+    select case (nb)
+
+    case (a3_neighb_highz)
+       bc_type = af_bc_dirichlet
+
+       do j = 1, nc
+          do i = 1, nc
+             rr = a3_r_cc(box, [i, j, 0])
+             rdist = norm2(rr(1:2) - field_dropoff_pos(1:2))
+             rdist = (rdist - field_dropoff_radius) / &
+                  (field_dropoff_relwidth * ST_domain_len)
+
+             if (rdist < 0) then
+                box%cc(i, j, nc+1, iv) = field_voltage
+             else
+                box%cc(i, j, nc+1, iv) = field_voltage * &
+                     max(0.0_dp, (1 - rdist))
+             end if
+          end do
+       end do
+
+    case default
+       call field_bc_homogeneous(box, nb, iv, bc_type)
+    end select
+  end subroutine field_bc_dropoff_lin
+
+  subroutine field_bc_dropoff_log(box, nb, iv, bc_type, i_eps)
+    type(box3_t), intent(inout) :: box
+    integer, intent(in)          :: nb      ! Direction for the boundary condition
+    integer, intent(in)          :: iv      ! Index of variable
+    integer, intent(out)         :: bc_type ! Type of boundary condition
+    integer, intent(in), optional   :: i_eps
+    integer                      :: nc, i
+
+    integer                      :: j
+
+    real(dp)                     :: rr(3), rdist, tmp
+
+    nc = box%n_cell
+    tmp = field_dropoff_relwidth * ST_domain_len
+
+    select case (nb)
+
+    case (a3_neighb_highz)
+       bc_type = af_bc_dirichlet
+
+       do j = 1, nc
+          do i = 1, nc
+             rr = a3_r_cc(box, [i, j, 0])
+             rdist = norm2(rr(1:2) - field_dropoff_pos(1:2))
+
+             if (rdist < field_dropoff_radius) then
+                box%cc(i, j, nc+1, iv) = field_voltage
+             else
+                box%cc(i, j, nc+1, iv) = field_voltage * &
+                     log(1 + tmp/rdist) / log(1 + tmp/field_dropoff_radius)
+             end if
+          end do
+       end do
+
+    case default
+       call field_bc_homogeneous(box, nb, iv, bc_type)
+    end select
+  end subroutine field_bc_dropoff_log
+
+  !> Compute electric field from electrical potential
+  subroutine field_from_potential(box)
+    type(box3_t), intent(inout) :: box
+    integer                      :: nc, i, j, k
+    real(dp)                     :: inv_dr, Ex, Ey
+
+    real(dp)                     :: Ez
+
+    nc     = box%n_cell
+    inv_dr = 1 / box%dr
+
+    do k = 1, nc+1; do j = 1, nc+1; do i = 1, nc+1
+
+       box%fc(i, j, k, 1, electric_fld) = -gradient_3d(box, 2, [i, j, k], 1, i_phi)
+       box%fc(i, j, k, 2, electric_fld) = -gradient_3d(box, 2, [i, j, k], 3, i_phi)
+       box%fc(i, j, k, 3, electric_fld) = -gradient_3d(box, 2, [i, j, k], 5, i_phi)
+
+    end do; end do; end do
+
+    do k = 1, nc; do j = 1, nc; do i = 1, nc
+
+       Ex = -0.5_dp*(gradient_3d(box, 2, [i, j, k], 1, i_phi) + gradient_3d(box, 2, [i, j, k], 2, i_phi))
+       Ey = -0.5_dp*(gradient_3d(box, 2, [i, j, k], 3, i_phi) + gradient_3d(box, 2, [i, j, k], 4, i_phi))
+       Ez = -0.5_dp*(gradient_3d(box, 2, [i, j, k], 5, i_phi) + gradient_3d(box, 2, [i, j, k], 6, i_phi))
+       box%cc(i, j, k, i_electric_fld) = sqrt(Ex**2+Ey**2+Ez**2)
+
+    end do; end do; end do
+
+  end subroutine field_from_potential
+
+end module m_field_3d
+
---
+++ src/m_flux_schemes_3d.f90
@@ -0,0 +1,186 @@
+!> Module containing a couple simple flux schemes for scalar advection/diffusion
+!> problems
+!>
+!> @todo Explain multidimensional index for flux, velocity
+module m_flux_schemes_3d
+  use m_streamer
+  use m_units_constants
+  use m_a3_all
+  
+  implicit none
+  private
+
+  public :: flux_diff_3d
+  public :: flux_koren_1d, flux_koren_2d, flux_koren_3d
+  
+  public :: gradient_3d
+
+contains
+
+  !> Compute diffusive flux (second order)
+  !> Compute diffusive flux (second order)
+  subroutine flux_diff_3d(box, dc, iv)
+    type(box3_t), intent(in)  :: box
+    integer, intent(in)        :: iv
+    real(dp), intent(inout)    :: dc(1:box%n_cell+1, 1:box%n_cell+1, 1:box%n_cell+1, 3)
+    integer                    :: nc, d, i, j, k
+
+    nc = box%n_cell
+
+    do k = 1,  nc+1; do j = 1,  nc+1; do i = 1,  nc+1
+       do d = 1, 3  
+          dc(i, j, k, d) = - dc(i, j, k, d) * gradient_3d(box, 1, [i, j, k], 2*d-1, iv)
+       end do
+    end do; end do; end do
+  end subroutine flux_diff_3d
+
+  !> Compute flux according to Koren limiter
+  subroutine flux_koren_1d(cc, v, nc, inv_dr)
+    integer, intent(in)     :: nc               !< Number of cells
+    real(dp), intent(in)    :: cc(-1:nc+2) !< Cell-centered values
+    !> Input: velocities at interfaces, output: fluxes
+    real(dp), intent(inout) :: v(1:nc+1)
+    real(dp), intent(in)    :: inv_dr
+    real(dp)                :: gradp, gradc, gradn
+    integer                 :: n
+
+    
+    do n = 1, nc+1
+       gradc = cc(n) - cc(n-1)  ! Current gradient
+       if (v(n) < 0.0_dp) then
+         gradn = cc(n+1) - cc(n)  ! Next gradient
+         v(n) = v(n) * (cc(n) - koren_mlim(gradc, gradn))
+       else                    
+         gradp = cc(n-1) - cc(n-2)  ! Previous gradient
+         v(n) = v(n) * (cc(n-1) + koren_mlim(gradc, gradp))
+       end if
+    end do
+
+  end subroutine flux_koren_1d
+
+  !> Compute flux according to Koren limiter
+  subroutine flux_koren_2d(cc, v, nc, inv_dr)
+    integer, intent(in)     :: nc                      !< Number of cells
+    real(dp), intent(in)    :: cc(-1:nc+2, -1:nc+2)  !< Cell-centered values
+    !> Input: velocities at interfaces, output: fluxes
+    real(dp), intent(inout) :: v(1:nc+1, 1:nc+1, 2)
+    real(dp), intent(in)    :: inv_dr
+    real(dp)                :: cc_1d(-1:nc+2), v_1d(1:nc+1)
+    integer                 :: n
+
+    do n = 1, nc
+       ! x-fluxes
+       call flux_koren_1d(cc(:, n), v(:, n, 1), nc, inv_dr)
+       ! y-fluxes (use temporary variables for efficiency)
+       cc_1d    = cc(n, :)
+       v_1d     = v(n, :, 2)
+       call flux_koren_1d(cc_1d, v_1d, nc, inv_dr)
+       v(n, :, 2) = v_1d     ! Copy result
+    end do
+  end subroutine flux_koren_2d
+
+  !> Compute flux according to Koren limiter
+  subroutine flux_koren_3d(cc, v, nc, inv_dr)
+    integer, intent(in)     :: nc                      !< Number of cells
+    !> Cell-centered values
+    real(dp), intent(in)    :: cc(-1:nc+2, -1:nc+2, -1:nc+2)  !< Cell-centered values
+    !> Input: velocities at interfaces, output: fluxes
+    real(dp), intent(inout) :: v(1:nc+1, 1:nc+1, 1:nc+1, 3)
+    real(dp), intent(in)    :: inv_dr
+    real(dp)                :: cc_1d(-1:nc+2), v_1d(1:nc+1)
+    integer                 :: n, m
+
+    do m = 1, nc
+       do n = 1, nc
+          ! x-fluxes
+          call flux_koren_1d(cc(:, n, m), v(:, n, m, 1), nc, inv_dr)
+
+          ! y-fluxes (use temporary variables for efficiency)
+          cc_1d    = cc(n, :, m)
+          v_1d     = v(n, :, m, 2)
+          call flux_koren_1d(cc_1d, v_1d, nc, inv_dr)
+          v(n, :, m, 2) = v_1d ! Copy result
+
+          ! z-fluxes (use temporary variables for efficiency)
+          cc_1d    = cc(n, m, :)
+          v_1d     = v(n, m, :, 3)
+          call flux_koren_1d(cc_1d, v_1d, nc, inv_dr)
+          v(n, m, :, 3) = v_1d ! Copy result
+       end do
+    end do
+  end subroutine flux_koren_3d
+  
+  
+
+  !> Modified implementation of Koren limiter, to avoid division and the min/max
+  !> functions, which can be problematic / expensive. In most literature, you
+  !> have r = a / b (ratio of gradients). Then the limiter phi(r) is multiplied
+  !> with b. With this implementation, you get phi(r) * b
+  elemental function koren_mlim(a, b) result(bphi)
+    real(dp), intent(in) :: a  !< Density gradient (numerator)
+    real(dp), intent(in) :: b  !< Density gradient (denominator)
+    real(dp), parameter  :: sixth = 1/6.0_dp
+    real(dp)             :: bphi, aa, ab
+
+    aa = a * a
+    ab = a * b
+
+    if (ab <= 0) then
+       ! a and b have different sign or one of them is zero, so r is either 0,
+       ! inf or negative (special case a == b == 0 is ignored)
+       bphi = 0
+    else if (aa <= 0.25_dp * ab) then
+       ! 0 < a/b <= 1/4, limiter has value a/b
+       bphi = a
+    else if (aa <= 2.5_dp * ab) then
+       ! 1/4 < a/b <= 2.5, limiter has value (1+2*a/b)/6
+       bphi = sixth * (b + 2*a)
+    else
+       ! (1+2*a/b)/6 >= 1, limiter has value 1
+       bphi = b
+    end if
+  end function koren_mlim
+  
+  !> Return the gradient off cell centered variable iv in a direction dir from inside cell ix
+  !> IMPORTANT : for cc_num 2 it is assumed here that s_iv(1) is negative charge and s_iv(2) positive
+  function gradient_3d(box, cc_num, ix, dir, iv) result(grad)
+    type(box3_t), intent(in)         :: box
+    integer, intent(in)               :: cc_num !< 2 if cc is assumed continuous 1 otherwise
+    integer, intent(in)               :: ix(3), dir, iv
+    real(dp)                          :: grad, inv_dr, s_C, harm_ep, med, fac
+    integer                           :: n_ix(3)
+    
+    n_ix      = a3_neighb_dix(:, dir)
+    inv_dr    = 1/box%dr
+    med       = a3_harm_w(1.0_dp, ST_epsilon_die, 0.5_dp)
+ 
+
+    if (a3_border(box%cc(ix(1), ix(2), ix(3), i_eps), box%cc(ix(1)+n_ix(1), ix(2)+n_ix(2), ix(3)+n_ix(3), i_eps), med)) then
+      if (cc_num == 2) then
+        harm_ep = 2.0_dp*box%cc(ix(1), ix(2), ix(3), i_eps)*box%cc(ix(1)+n_ix(1), ix(2)+n_ix(2), ix(3)+n_ix(3), i_eps)/&
+                  (box%cc(ix(1), ix(2), ix(3), i_eps)+box%cc(ix(1)+n_ix(1), ix(2)+n_ix(2), ix(3)+n_ix(3), i_eps))           
+        s_C     = box%fc(ix(1)+n_ix(1)*a3_neighb_high_01(dir), ix(2)+n_ix(2)*a3_neighb_high_01(dir),&
+                  ix(3)+n_ix(3)*a3_neighb_high_01(dir), a3_neighb_dim(dir), sigma_rhs)
+        grad    = (inv_dr*a3_neighb_high_pm(dir)*(box%cc(ix(1)+n_ix(1), ix(2)+n_ix(2), ix(3)+n_ix(3), iv) -&
+                  box%cc(ix(1), ix(2), ix(3), iv)) - 0.5_dp*s_C/box%cc(ix(1)+n_ix(1), ix(2)+n_ix(2), ix(3)+n_ix(3), i_eps))&
+                  * harm_ep/box%cc(ix(1), ix(2), ix(3), i_eps)
+      else if (cc_num == 1) then
+        fac   = 0.5_dp * box%fc(ix(1)+n_ix(1)*a3_neighb_high_01(dir), ix(2)+n_ix(2)*a3_neighb_high_01(dir),&
+                ix(3)+n_ix(3)*a3_neighb_high_01(dir), a3_neighb_dim(dir), sigma_rhs)/ &
+                (box%fc(ix(1)+n_ix(1)* a3_neighb_high_01(dir), ix(2)+n_ix(2)*a3_neighb_high_01(dir), &
+                ix(3)+n_ix(3)*a3_neighb_high_01(dir), a3_neighb_dim(dir), sigma_rhs) + &
+                box%cc(ix(1)+n_ix(1), ix(2)+n_ix(2), ix(3)+n_ix(3), iv)/inv_dr + epsilon(1.0_dp))
+        
+        grad  = inv_dr*a3_neighb_high_pm(dir)*(box%fc(ix(1)+n_ix(1)*a3_neighb_high_01(dir), ix(2)+&
+                n_ix(2)*a3_neighb_high_01(dir), ix(3)+n_ix(3)*a3_neighb_high_01(dir), a3_neighb_dim(dir), sigma_rhs)*inv_dr &
+                +box%cc(ix(1)+n_ix(1), ix(2)+n_ix(2), ix(3)+n_ix(3), iv)-box%cc(ix(1), ix(2), ix(3), iv)) / (1-fac)
+      end if
+    else
+      grad      = inv_dr*a3_neighb_high_pm(dir)*(box%cc(ix(1)+n_ix(1), ix(2)+n_ix(2), ix(3)+n_ix(3), iv)-&
+                  box%cc(ix(1), ix(2), ix(3), iv))
+    end if
+
+  end function gradient_3d
+
+end module m_flux_schemes_3d
+
