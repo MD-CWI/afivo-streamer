d50f486489629c8d9cfe2d78cc40ebc01933f252
Jannis Teunissen <jannis@teunissen.net>
2019-02-01 18:21:20 +0100

    Add script for building Hypre (debug type for now)

---
+++ external_libraries/build_hypre.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Set names/directories
+target_dir=`pwd`"/hypre"
+hypre_tarname="hypre-2.11.2.tar.gz"
+hypre_dirname="hypre-2.11.2"
+build_dir="build"
+
+# Do compilation etc. in build directory
+mkdir -p ${build_dir}
+cd ${build_dir}
+
+# Extract
+if [ ! -d ${hypre_dirname} ]; then
+    tar -xzf ${hypre_tarname}
+fi
+
+# Configure
+cd ${hypre_dirname}/src
+./configure --with-openmp --without-MPI --with-print-errors --enable-debug --prefix=${target_dir}
+
+make -j
+make install
