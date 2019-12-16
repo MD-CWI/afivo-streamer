08f1c82855a7fa8bae214da26de72d64bd96bd6a
lixiaoran <lixiaoran1996@foxmail.com>
2019-12-16 14:48:00 +0100

    Delete unnecessary command files

--- programs/dielectric_2d/basheps.sh
+++
@@ -1,8 +0,0 @@
-mkdir -p ./output/final/eps/2.0
-./streamer positive_2d_left.cfg -output%name="output/final/eps/2.0/2.0eps" -seed_rel_r0="0.2625 1.0" -seed_rel_r1="0.2625 0.95"  -dielectric_eps=2.0
-
-mkdir -p ./output/final/eps/3.0
-./streamer positive_2d_left.cfg -output%name="output/final/eps/3.0/3.0eps" -seed_rel_r0="0.2625 1.0" -seed_rel_r1="0.2625 0.95"  -dielectric_eps=3.0  -end_time=15.0e-9
-
-mkdir -p ./output/final/eps/5.0
-./streamer positive_2d_left.cfg -output%name="output/final/eps/5.0/5.0eps" -seed_rel_r0="0.2625 1.0" -seed_rel_r1="0.2625 0.95"  -dielectric_eps=5.0 -end_time=12.0e-9
