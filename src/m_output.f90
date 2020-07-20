308c50d26589e445a3bfe416d0c53b2230dbabff
Jannis Teunissen <jannis@teunissen.net>
2020-07-20 17:01:42 +0200

    Users can now add variables to the log file

--- src/m_output.f90
+++ src/m_output.f90
@@ -269,18 +269,32 @@ contains
 
   subroutine output_log(tree, filename, out_cnt, wc_time)
     use m_field, only: field_voltage
+    use m_user_methods
     type(af_t), intent(in)       :: tree
     character(len=*), intent(in) :: filename
     integer, intent(in)          :: out_cnt !< Output number
     real(dp), intent(in)         :: wc_time !< Wallclock time
-    character(len=30), save      :: fmt
+    character(len=50), save      :: fmt
     integer                      :: my_unit
     real(dp)                     :: velocity, dt
     real(dp), save               :: prev_pos(NDIM) = 0
     real(dp)                     :: sum_elec, sum_pos_ion
     real(dp)                     :: max_elec, max_field, max_Er, min_Er
     type(af_loc_t)               :: loc_elec, loc_field, loc_Er
-    logical, save                :: first_time = .true.
+    integer                      :: i, n_reals, n_user_vars
+    character(len=name_len)      :: var_names(user_max_log_vars)
+    real(dp)                     :: var_values(user_max_log_vars)
+    logical, save                :: first_time     = .true.
+
+    if (associated(user_log_variables)) then
+       ! Set default names for the user variables
+       do i = 1, user_max_log_vars
+          write(var_names, "(A,I0)") "uservar_", i
+       end do
+       call user_log_variables(tree, n_user_vars, var_names, var_values)
+    else
+       n_user_vars = 0
+    end if
 
     call af_tree_sum_cc(tree, i_electron, sum_elec)
     call af_tree_sum_cc(tree, i_1pos_ion, sum_pos_ion)
@@ -296,14 +310,20 @@ contains
 
        open(newunit=my_unit, file=trim(filename), action="write")
 #if NDIM == 2
-       write(my_unit, *) "it time dt v sum(n_e) sum(n_i) ", &
-            "max(E) x y max(n_e) x y max(E_r) x y min(E_r) voltage ", &
-            " wc_time n_cells min(dx) highest(lvl)"
+       write(my_unit, "(A)", advance="no") "it time dt v sum(n_e) sum(n_i) &
+            &max(E) x y max(n_e) x y max(E_r) x y min(E_r) voltage &
+            &wc_time n_cells min(dx) highest(lvl)"
 #elif NDIM == 3
-       write(my_unit, *) "it time dt v sum(n_e) sum(n_i) ", &
-            "max(E) x y z max(n_e) x y z voltage ", &
-            "wc_time n_cells min(dx) highest(lvl)"
+       write(my_unit, "(A)", advance="no") "it time dt v sum(n_e) sum(n_i) &
+            &max(E) x y z max(n_e) x y z voltage &
+            &wc_time n_cells min(dx) highest(lvl)"
 #endif
+       if (associated(user_log_variables)) then
+          do i = 1, n_user_vars
+             write(my_unit, "(A)", advance="no") " "//trim(var_names(i))
+          end do
+       end if
+       write(my_unit, *) ""
        close(my_unit)
 
        ! Start with velocity zero
@@ -311,10 +331,17 @@ contains
     end if
 
 #if NDIM == 2
-    fmt = "(I6,17E16.8,I12,1E16.8,I3)"
+    n_reals = 17
 #elif NDIM == 3
-    fmt = "(I6,15E16.8,I12,1E16.8,I3)"
+    n_reals = 15
 #endif
+
+    if (associated(user_log_variables)) then
+       write(fmt, "(A,I0,A,I0,A)") "(I6,", n_reals, "E16.8,I12,1E16.8,I3,", &
+            n_user_vars, "E16.8)"
+    else
+       write(fmt, "(A,I0,A)") "(I6,", n_reals, "E16.8,I12,1E16.8,I3)"
+    end if
 
     velocity = norm2(af_r_loc(tree, loc_field) - prev_pos) / output_dt
     prev_pos = af_r_loc(tree, loc_field)
@@ -326,13 +353,15 @@ contains
          sum_pos_ion, max_field, af_r_loc(tree, loc_field), max_elec, &
          af_r_loc(tree, loc_elec), max_Er, af_r_loc(tree, loc_Er), min_Er, &
          field_voltage, &
-         wc_time, af_num_cells_used(tree), af_min_dr(tree),tree%highest_lvl
+         wc_time, af_num_cells_used(tree), af_min_dr(tree),tree%highest_lvl, &
+         var_values(1:n_user_vars)
 #elif NDIM == 3
     write(my_unit, fmt) out_cnt, global_time, dt, velocity, sum_elec, &
          sum_pos_ion, max_field, af_r_loc(tree, loc_field), max_elec, &
          af_r_loc(tree, loc_elec), field_voltage, &
          wc_time, af_num_cells_used(tree), &
-         af_min_dr(tree),tree%highest_lvl
+         af_min_dr(tree),tree%highest_lvl, &
+         var_values(1:n_user_vars)
 #endif
     close(my_unit)
 
--- src/m_user_methods.f90
+++ src/m_user_methods.f90
@@ -2,6 +2,7 @@
 !> This module contains all the methods that users can customize
 module m_user_methods
   use m_af_all
+  use m_types
 
   implicit none
   public
@@ -21,10 +22,16 @@ module m_user_methods
   !> To set the field amplitude manually
   procedure(field_func), pointer :: user_field_amplitude => null()
 
+  !> To write a custom log file
   procedure(log_subr), pointer :: user_write_log => null()
+
+  !> To add entries to the log file
+  procedure(log_vars), pointer :: user_log_variables => null()
 
   !> Whether to simulate the plasma fluid equations for the electrons
   procedure(bool_subr), pointer :: user_evolve_electrons => null()
+
+  integer, parameter :: user_max_log_vars = 20
 
   interface
      subroutine log_subr(tree, filename, out_cnt)
@@ -33,6 +40,14 @@ module m_user_methods
        character(len=*), intent(in) :: filename
        integer, intent(in)          :: out_cnt
      end subroutine log_subr
+
+     subroutine log_vars(tree, n_vars, var_names, var_values)
+       import
+       type(af_t), intent(in)                 :: tree
+       integer, intent(out)                   :: n_vars
+       character(len=name_len), intent(inout) :: var_names(user_max_log_vars)
+       real(dp), intent(inout)                :: var_values(user_max_log_vars)
+     end subroutine log_vars
 
      function gas_dens_func(box, IJK) result(dens)
        import
