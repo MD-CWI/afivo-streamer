bd28efb49f936abd009724b19e00ab1a31650094
Hemaditya Malla <hemadity@solano.md.cwi.nl>
2021-06-01 11:45:26 +0200

    WIP debug charge conservation in dielectric, fixed non-dielectric simulation error in m_field.f90

--- src/m_field.f90
+++ src/m_field.f90
@@ -348,10 +348,10 @@ contains
 
     ! Compute field from potential
     call mg_compute_phi_gradient(tree, mg, electric_fld, -1.0_dp, i_electric_fld)
-
+   if (ST_use_dielectric) then
     call dielectric_correct_field_fc(tree, diel, i_surf_dens, &
          electric_fld, i_phi, UC_eps0)
-
+   end if
     ! Set the field norm also in ghost cells
     call af_gc_tree(tree, [i_electric_fld])
   end subroutine field_compute
