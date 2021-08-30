36d826ed2905c5b8ecf155f09d8dd15260ba2476
Jannis Teunissen <jannis@teunissen.net>
2021-08-30 10:48:29 +0200

    Increase af_max_num_vars to 1024
    The cost of the fixed extra storage is pretty small

--- src/m_af_types.f90
+++ src/m_af_types.f90
@@ -17,7 +17,7 @@ module m_af_types
   integer, parameter :: af_min_lvl = 1
 
   !> Maximum number of variables
-  integer, parameter :: af_max_num_vars = 100
+  integer, parameter :: af_max_num_vars = 1024
 
   !> Value indicating you want to derefine a box
   integer, parameter :: af_rm_ref = -1
