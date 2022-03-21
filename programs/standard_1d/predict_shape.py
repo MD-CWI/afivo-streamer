f7f97721b905d3fd5e73d9676cd72bacf077879e
lixiaoran1996 <lixiaoran1996@foxmail.com>
2022-03-21 15:16:03 +0100

    small changes to predict_shape.py

--- programs/standard_1d/predict_shape.py
+++ programs/standard_1d/predict_shape.py
@@ -19,6 +19,7 @@ args = p.parse_args()
 
 f = np.load(args.dataset)
 all_data = f['X']
+N = args.N
 ix = args.ix
 
 # Input features
@@ -58,26 +59,29 @@ print('Save model:', model_name)
 
 n_t = all_data.shape[0]
 n_shift_max = all_data.shape[2]//4
-new_n_x = 3*all_data.shape[2]//4 - 10
+new_n_x = 3*all_data.shape[2]//4 - N
 data_new = np.zeros((n_t, all_data.shape[1], new_n_x))
 
 ix_start = np.random.random_integers(0, n_shift_max, n_t)
 for i in range(n_t):
     data_new[i,:,:] = all_data[i,:,ix_start[i]:ix_start[i]+new_n_x]
 
-fig, ax = plt.subplots(3)
+fig, ax = plt.subplots(4)
 for x in data_new[:,2,:]:
     ax[0].plot(x)
+    ax[0].set_xlim(0,data_new.shape[2]//N*N)
+    ax[0].set_title('shifted data')
 print('Plot data_new rhs')
 
 # Get a low resolution matrix
-N = args.N
 data_small = np.zeros((data_new.shape[0], data_new.shape[1], int(data_new.shape[2]/N)))
-for i in range(int(data_new.shape[2]/N)):
+for i in range(data_new.shape[2]//N):
     data_small[:,:,i] = np.average(data_new[:,:,N*i:N*i+N],axis=2)
 
 for x in data_small[:,2,:]:
     ax[1].plot(x)
+    ax[1].set_xlim(0,data_new.shape[2]//N)
+    ax[1].set_title('low resolution data')
 print('Plot data_small rhs')
 
 '''
@@ -107,13 +111,18 @@ loc_orig = np.zeros(data_small.shape[0], dtype=int)
 
 # find the location of the peak value from low resolution data
 for i in range(data_small.shape[0]):
-    # f = interpolate.interp1d(data_small[i, 3, :], data_small[i, 2, :],
-                             # kind='quadratic', bounds_error=False)
-    f = interpolate.Rbf(data_small[i, 3, :], data_small[i, 2, :],
-                        bounds_error=False)
+    f = interpolate.interp1d(data_small[i, 3, :], data_small[i, 2, :],
+                              kind='quadratic', bounds_error=False)
+    #f = interpolate.Rbf(data_small[i, 3, :], data_small[i, 2, :],
+                        #bounds_error=False)
     ax[2].plot(f(data_new[i, 3, :]))
+    ax[2].set_xlim(0,data_new.shape[2]//N*N)
+    ax[2].set_title('interpolated data')
     loc_orig[i] = np.nanargmax(f(data_new[i, 3, :]))
 loc_shift = all_data.shape[2]//2 - loc_orig
+
+ax[3].plot(loc_shift-ix_start)
+ax[3].set_title('error of the peak location')
 
 print('Mean shift error: ', (loc_shift-ix_start).mean())
 print('Mean absolute shift error: ', np.abs(loc_shift-ix_start).mean())
