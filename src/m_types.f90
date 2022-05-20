86ab16de71c9fa793293098360cfaa086b490fb6
Jannis Teunissen <jannis@teunissen.net>
2022-05-20 15:19:08 +0200

    Add constant "undefined_real"

--- src/m_types.f90
+++ src/m_types.f90
@@ -4,7 +4,13 @@ module m_types
   implicit none
   public
 
+  integer, parameter, private :: dp = kind(0.0d0)
+
+  !> Undefined string
   character(len=*), parameter :: undefined_str = "UNDEFINED"
+
+  !> Undefined number
+  real(dp), parameter :: undefined_real = -1e100_dp
 
   !> Default length of strings
   integer, parameter :: string_len = 200
