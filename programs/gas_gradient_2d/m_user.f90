6204f289383b4234feb9eea1cc3b885eb5f6d6fe
Jannis Teunissen <jannis@teunissen.net>
2023-01-09 17:27:25 +0100

    Add spherical gas density gradient to example

--- programs/gas_gradient_2d/m_user.f90
+++ programs/gas_gradient_2d/m_user.f90
@@ -13,17 +13,31 @@ module m_user
   ! Public methods
   public :: user_initialize
 
-  real(dp)          :: density_ratio = 0.5_dp
-  real(dp)          :: shock_width = 0.02_dp
-  real(dp)          :: line_coeff(NDIM+1) = 0.0_dp
+  real(dp) :: density_ratio       = 0.5_dp
+  real(dp) :: shock_width         = 0.02_dp
+  real(dp) :: line_coeff(NDIM+1)  = 0.0_dp
+  real(dp) :: sphere_center(NDIM) = 0.5_dp
+  real(dp) :: sphere_radius       = 0.1_dp
 
 contains
 
   subroutine user_initialize(cfg, tree)
     type(CFG_t), intent(inout) :: cfg
-    type(af_t), intent(inout) :: tree
+    type(af_t), intent(inout)  :: tree
+    character(len=100)         :: gradient_type
 
-    user_gas_density => gas_density
+    gradient_type = "line"
+    call CFG_add_get(cfg, "gradient_type", gradient_type, &
+         "What type of gas gradient to use (line, sphere)")
+
+    select case (gradient_type)
+    case ("line")
+       user_gas_density => gas_density_line
+    case ("sphere")
+       user_gas_density => gas_density_sphere
+    case default
+       error stop "Unknown gradient_type"
+    end select
 
     call CFG_add_get(cfg, "density_ratio", density_ratio, &
          "Density ratio (<= 1)")
@@ -31,28 +45,55 @@ contains
          "Shock width (relative to domain size)")
     call CFG_add_get(cfg, "line_coeff", line_coeff, &
          "Coefficients a, b, c of a line a + bx + cy = 0")
+    call CFG_add_get(cfg, "sphere_center", sphere_center, &
+         "Center (relative to domain) of sphere")
+    call CFG_add_get(cfg, "sphere_radius", sphere_radius, &
+         "Radius (relative to domain) of sphere")
 
   end subroutine user_initialize
 
-  real(dp) function gas_density(box, IJK)
+  !> Gas density is different on two sides of a line
+  real(dp) function gas_density_line(box, IJK)
     use m_gas
     type(box_t), intent(in) :: box
     integer, intent(in)     :: IJK
     real(dp)                :: r_rel(NDIM), q, tmp
 
     ! Get relative coordinates in the range [0, 1]
-    r_rel = (af_r_cc(box, [IJK]) - ST_domain_origin)/ ST_domain_len
+    r_rel = (af_r_cc(box, [IJK]) - ST_domain_origin)/ST_domain_len
     q = line_coeff(1) + sum(line_coeff(2:) * r_rel)
 
     if (q < -shock_width) then
-       gas_density = gas_number_density
+       gas_density_line = gas_number_density
     else if (q > shock_width) then
-       gas_density = gas_number_density * density_ratio
+       gas_density_line = gas_number_density * density_ratio
     else
        ! Linear interpolation
        tmp = (q + shock_width) / (2 * shock_width)
-       gas_density = gas_number_density * (1 + (density_ratio-1) * tmp)
+       gas_density_line = gas_number_density * (1 + (density_ratio-1) * tmp)
     end if
-  end function gas_density
+  end function gas_density_line
+
+  !> Gas density is different inside and outside sphere
+  real(dp) function gas_density_sphere(box, IJK)
+    use m_gas
+    type(box_t), intent(in) :: box
+    integer, intent(in)     :: IJK
+    real(dp)                :: q, tmp
+
+    ! Get relative coordinates with regard to sphere center
+    q = norm2((af_r_cc(box, [IJK]) - ST_domain_origin)/ST_domain_len &
+         - sphere_center)
+
+    if (q < sphere_radius - shock_width) then
+       gas_density_sphere = gas_number_density
+    else if (q > sphere_radius + shock_width) then
+       gas_density_sphere = gas_number_density * density_ratio
+    else
+       ! Linear interpolation
+       tmp = (q - sphere_radius + shock_width) / (2 * shock_width)
+       gas_density_sphere = gas_number_density * (1 + (density_ratio-1) * tmp)
+    end if
+  end function gas_density_sphere
 
 end module m_user
