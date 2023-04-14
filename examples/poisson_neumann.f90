08d919761289ee078725b4454538378811fbcb8f
Jannis Teunissen <jannis@teunissen.net>
2023-04-14 13:07:58 +0200

    Add cylindrical option to poisson_neumann example

--- examples/poisson_neumann.f90
+++ examples/poisson_neumann.f90
@@ -2,7 +2,7 @@
 !> \example poisson_neumann.f90
 !>
 !> Example showing how to use multigrid in combination with Neumann boundary
-!> conditions.
+!> conditions. There is also a test case for a cylindrical geometry.
 program poisson_neumann
   use m_af_all
   use m_gaussians
@@ -11,44 +11,46 @@ program poisson_neumann
 
   integer, parameter :: box_size     = 8
   integer, parameter :: n_iterations = 10
-  integer            :: i_phi
-  integer            :: i_rhs
-  integer            :: i_tmp
   integer            :: i_err
 
-  type(af_t)        :: tree
+  type(af_t)         :: tree
   type(ref_info_t)   :: refine_info
   integer            :: mg_iter
-  character(len=100) :: fname
-  type(mg_t)       :: mg
-  integer            :: count_rate,t_start,t_end
+  character(len=100) :: fname, arg
+  type(mg_t)         :: mg
+  integer            :: count_rate, t_start, t_end, coord
+  logical            :: cylindrical = .false.
 
   print *, "Running poisson_neumann_" // DIMNAME // ""
   print *, "Number of threads", af_get_max_threads()
 
-  call af_add_cc_variable(tree, "phi", ix=i_phi)
-  call af_add_cc_variable(tree, "rhs", ix=i_rhs)
-  call af_add_cc_variable(tree, "tmp", ix=i_tmp)
+  if (command_argument_count() > 0) then
+     call get_command_argument(1, arg)
+     read (arg, *) cylindrical
+  end if
+
+  if (cylindrical) then
+     coord = af_cyl
+  else
+     coord = af_xyz
+  end if
+
+  call af_add_cc_variable(tree, "phi", ix=mg%i_phi)
+  call af_add_cc_variable(tree, "rhs", ix=mg%i_rhs)
+  call af_add_cc_variable(tree, "tmp", ix=mg%i_tmp)
   call af_add_cc_variable(tree, "err", ix=i_err)
 
   ! Initialize tree
   call af_init(tree, & ! Tree to initialize
        box_size, &     ! A box contains box_size**DIM cells
        [DTIMES(1.0_dp)], &
-       [DTIMES(box_size)])
+       [DTIMES(box_size)], &
+       coord=coord)
 
   call system_clock(t_start, count_rate)
   do
-     ! For each box, set the initial conditions
      call af_loop_box(tree, set_initial_condition)
-
-     ! This updates the refinement of the tree, by at most one level per call.
-     ! The second argument is a subroutine that is called for each box that can
-     ! be refined or derefined, and it should set refinement flags. Information
-     ! about the changes in refinement are returned in the third argument.
      call af_adjust_refinement(tree, refine_routine, refine_info, 0)
-
-     ! If no new boxes have been added, exit the loop
      if (refine_info%n_add == 0) exit
   end do
   call system_clock(t_end, count_rate)
@@ -58,16 +60,13 @@ program poisson_neumann
 
   call af_print_info(tree)
 
-  ! Initialize the multigrid options. This performs some basics checks and sets
-  ! default values where necessary.
-  mg%i_phi        = i_phi       ! Solution variable
-  mg%i_rhs        = i_rhs       ! Right-hand side variable
-  mg%i_tmp        = i_tmp       ! Variable for temporary space
-  mg%sides_bc     => sides_bc   ! Method for boundary conditions
+   ! Method for boundary conditions
+  if (cylindrical) then
+     mg%sides_bc => sides_bc_cyl
+  else
+     mg%sides_bc => sides_bc
+  end if
 
-  ! This routine does not initialize the multigrid fields boxes%i_phi,
-  ! boxes%i_rhs and boxes%i_tmp. These fileds will be initialized at the
-  ! first call of mg_fas_fmg
   call mg_init(tree, mg)
 
   print *, "Multigrid iteration | max residual | max error"
@@ -75,7 +74,6 @@ program poisson_neumann
 
   do mg_iter = 1, n_iterations
      call mg_fas_fmg(tree, mg, set_residual=.true., have_guess=(mg_iter>1))
-     ! call mg_fas_vcycle(tree, mg, tree%highest_lvl, set_residual=.true.)
 
      call af_loop_box(tree, set_error)
 
@@ -108,7 +106,13 @@ contains
   subroutine set_initial_condition(box)
     type(box_t), intent(inout) :: box
 
-    box%cc(DTIMES(:), i_rhs) = 0.0_dp
+    if (cylindrical) then
+       ! Use manufactured solution, phi(r, z) = r**2. After applying Laplacian
+       ! in cylindrical coordinates, this gives rhs = 4
+       box%cc(DTIMES(:), mg%i_rhs) = 4.0_dp
+    else
+       box%cc(DTIMES(:), mg%i_rhs) = 0.0_dp
+    end if
   end subroutine set_initial_condition
 
   ! Set the error compared to the analytic solution
@@ -120,9 +124,19 @@ contains
     nc = box%n_cell
     do KJI_DO(1,nc)
        rr = af_r_cc(box, [IJK])
-       box%cc(IJK, i_err) = box%cc(IJK, i_phi) - rr(1)
+       box%cc(IJK, i_err) = box%cc(IJK, mg%i_phi) - solution(rr(1))
     end do; CLOSE_DO
   end subroutine set_error
+
+  real(dp) elemental function solution(x)
+    real(dp), intent(in) :: x
+
+    if (cylindrical) then
+       solution = x**2
+    else
+       solution = x
+    end if
+  end function solution
 
   ! This routine sets boundary conditions for a box
   subroutine sides_bc(box, nb, iv, coords, bc_val, bc_type)
@@ -149,5 +163,32 @@ contains
        bc_val = 0.0_dp
     end select
   end subroutine sides_bc
+
+  ! Boundary conditions for a cylindrical solution
+  subroutine sides_bc_cyl(box, nb, iv, coords, bc_val, bc_type)
+    type(box_t), intent(in) :: box
+    integer, intent(in)     :: nb
+    integer, intent(in)     :: iv
+    real(dp), intent(in)    :: coords(NDIM, box%n_cell**(NDIM-1))
+    real(dp), intent(out)   :: bc_val(box%n_cell**(NDIM-1))
+    integer, intent(out)    :: bc_type
+    integer                 :: nc
+
+    nc = box%n_cell
+
+    ! Below the solution is specified in the approriate ghost cells
+    select case (nb)
+    case (af_neighb_lowx)             ! Lower-x direction
+       bc_type = af_bc_neumann
+       bc_val = 0.0_dp
+    case (af_neighb_highx)             ! Higher-x direction
+       bc_type = af_bc_neumann
+       bc_val = 2.0_dp
+    case default
+       ! Have to specify some non-Neumann conditions
+       bc_type = af_bc_dirichlet
+       bc_val = solution(coords(1, :))
+    end select
+  end subroutine sides_bc_cyl
 
 end program
