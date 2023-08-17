9a2e633339a33b695b72561398b57703df04e86f
Jannis Teunissen <jannis@teunissen.net>
2023-08-17 17:26:04 +0200

    Small improvements to plot_transport_data_summary.py

--- tools/plot_transport_data_summary.py
+++ tools/plot_transport_data_summary.py
@@ -15,14 +15,15 @@ p.add_argument("-SI_field", action='store_true',
                help="Use electric field in V/m rather than Td")
 args = p.parse_args()
 
+tdata = pd.read_csv(args.summary_file, delim_whitespace=True)
+
 if args.SI_field:
-    tdata = pd.read_csv(args.summary_file, delim_whitespace=True, index_col=1)
+    tdata.set_index('E[V/m]', inplace=True)
     tdata.drop(columns='E/N[Td]', inplace=True)
 else:
-    tdata = pd.read_csv(args.summary_file, delim_whitespace=True, index_col=0)
+    tdata.set_index('E/N[Td]', inplace=True)
     tdata.drop(columns='E[V/m]', inplace=True)
 
-fig, ax = plt.subplots(figsize=(10, 10))
-tdata.plot(subplots=True, layout=(-1, 2), ax=ax, sharex=True)
+tdata.plot(subplots=True, layout=(-1, 2), sharex=True, figsize=(10, 10))
 
 plt.show()
