0529cae4c67137467a43c1c073342565fb492bc6
Jannis Teunissen <jannis@teunissen.net>
2023-09-11 15:40:32 +0200

    Allow to plot multiple transport data summary files

--- tools/plot_transport_data_summary.py
+++ tools/plot_transport_data_summary.py
@@ -10,20 +10,26 @@ import matplotlib.pyplot as plt
 
 p = argparse.ArgumentParser(
     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-p.add_argument("summary_file", type=str, help="File <simulation>_summary.txt")
+p.add_argument("summary_file", type=str, nargs='+',
+               help="File <simulation>_summary.txt")
 p.add_argument("-SI_field", action='store_true',
                help="Use electric field in V/m rather than Td")
 args = p.parse_args()
 
-tdata = pd.read_csv(args.summary_file, delim_whitespace=True)
+all_data = [pd.read_csv(f, delim_whitespace=True) for f in args.summary_file]
 
 if args.SI_field:
-    tdata.set_index('E[V/m]', inplace=True)
-    tdata.drop(columns='E/N[Td]', inplace=True)
+    for x in all_data:
+        x.set_index('E[V/m]', inplace=True)
+        x.drop(columns='E/N[Td]', inplace=True)
 else:
-    tdata.set_index('E/N[Td]', inplace=True)
-    tdata.drop(columns='E[V/m]', inplace=True)
+    for x in all_data:
+        x.set_index('E/N[Td]', inplace=True)
+        x.drop(columns='E[V/m]', inplace=True)
 
-tdata.plot(subplots=True, layout=(-1, 2), sharex=True, figsize=(10, 10))
+ax = all_data[0].plot(subplots=True, layout=(-1, 2),
+                      sharex=True, figsize=(10, 10))
+for x in all_data[1:]:
+    x.plot(subplots=True, layout=(-1, 2), sharex=True, figsize=(10, 10), ax=ax)
 
 plt.show()
