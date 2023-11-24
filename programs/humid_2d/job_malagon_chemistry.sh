414e452bedb3476cdd3468a43ac5b18936b93eec
Baohong Guo <Baohong.Guo@cwi.nl>
2023-11-24 17:04:11 +0100

    update job script

---
+++ programs/humid_2d/job_malagon_chemistry.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --nodes=1	     # number of nodes on which to run
+#SBATCH --ntasks=1	     # number of tasks to run
+#SBATCH --cpus-per-task=128    # number of cpus required per task
+#SBATCH --partition=rome	     # partition requested
+#SBATCH --time=2-00:00:00	     # wall-clock time limit
+
+module load 2022
+module load GCC/11.3.0
+module load parallel/20220722-GCCcore-11.3.0
+
+export OMP_NUM_THREADS=1
+export GFORTRAN_UNBUFFERED_PRECONNECTED=y
+
+parallel --jobs 120 --ungroup < d200mm_sensitivity_malagon_0.03H2O_commands.txt
